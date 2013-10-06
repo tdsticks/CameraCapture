288999efd1576e2011f8c36160704d30022c8885
Steve Stickel <tdsticks@gmail.com>
2013-10-06 02:35:39 -0400

    updated raspistill settings

--- camera_capture.py
+++ camera_capture.py
@@ -9,8 +9,8 @@ from PIL import Image
 # Threshold (how much a pixel has to change by to be marked as "changed")
 # Sensitivity (how many changed pixels before capturing an image)
 # ForceCapture (whether to force an image to be captured every forceCaptureTime seconds)
-threshold = 10
-sensitivity = 20
+threshold = 20 #10
+sensitivity = 40 #20
 forceCapture = True
 forceCaptureTime = 60 * 60 # Once an hour
 
@@ -19,24 +19,35 @@ saveWidth = 1280
 saveHeight = 960
 diskSpaceToReserve = 40 * 1024 * 1024 # Keep 40 mb free on disk
 
+
 # Capture a small test image (for motion detection)
 def captureTestImage():
-    command = "raspistill -w %s -h %s -t 0 -e bmp -o -" % (100, 75)
+
+    #command = "raspistill -w %s -h %s -t 0 -e bmp -o -" % (100, 75)
+    command = "raspistill -w %s -h %s -ex auto -awb auto -t 0 -e bmp -o -" % (100, 75)
+
     imageData = StringIO.StringIO()
     imageData.write(subprocess.check_output(command, shell=True))
     imageData.seek(0)
+
     im = Image.open(imageData)
     buffer = im.load()
     imageData.close()
+
     return im, buffer
+
 
 # Save a full size image to disk
 def saveImage(width, height, diskSpaceToReserve):
     keepDiskSpaceFree(diskSpaceToReserve)
     time = datetime.now()
     filename = "~/camera/capture-%04d%02d%02d-%02d%02d%02d.jpg" % (time.year, time.month, time.day, time.hour, time.minute, time.second)
-    subprocess.call("raspistill -w 1296 -h 972 -t 0 -e jpg -q 15 -o %s" % filename, shell=True)
+    
+    #subprocess.call("raspistill -w 1296 -h 972 -t 0 -e jpg -q 15 -o %s" % filename, shell=True)
+    subprocess.call("raspistill -w 1296 -h 972 -ex auto -awb auto -n -t 0 -e jpg -q 15 -o %s" % filename, shell=True)
+    
     print "Captured %s" % filename
+
 
 # Keep free space above given level
 def keepDiskSpaceFree(bytesToReserve):
@@ -48,17 +59,21 @@ def keepDiskSpaceFree(bytesToReserve):
                 if (getFreeSpace() > bytesToReserve):
                     return
 
+
 # Get available disk space
 def getFreeSpace():
     st = os.statvfs(".")
     du = st.f_bavail * st.f_frsize
     return du
-        
+     
+
 # Get first image
 image1, buffer1 = captureTestImage()
 
+
 # Reset last capture time
 lastCapture = time.time()
+
 
 while (True):
 
@@ -66,7 +81,9 @@ while (True):
     image2, buffer2 = captureTestImage()
 
     # Count changed pixels
+
     changedPixels = 0
+
     for x in xrange(0, 100):
         for y in xrange(0, 75):
             # Just check green channel as it's the highest quality channel
